f09509c65e089f895e094a9ad76b62c00a4f154c
dejan <31327026+Lija321@user.noreply.github.com>
2025-08-20 15:50:47 +0200

    #25 added visualizer to package path

--- api/src/api/services/__init__.py
+++ api/src/api/services/__init__.py
@@ -1,4 +1,5 @@
 from .plugin import Plugin
 from .data_source_plugin import DataSourcePlugin
+from .visualizer import Visualizer
 
-__all__ = ["Plugin", "DataSourcePlugin"]
+__all__ = ["Plugin", "DataSourcePlugin", "Visualizer"]
