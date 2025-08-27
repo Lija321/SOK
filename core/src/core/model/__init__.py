6f64d00466a719b5c05967a2ba5584c8f5b9bc5c
dejan <31327026+Lija321@user.noreply.github.com>
2025-08-27 16:13:43 +0200

    #14 exposed basefilter in the __init__.py file

---
+++ core/src/core/model/__init__.py
@@ -0,0 +1,7 @@
+from .base_filter import BaseFilter
+from .filter import Filter
+from .search import Search
+from .workspace import Workspace
+from .command_processor import *
+
+__all__ = ['BaseFilter', 'Filter', 'Search', 'Workspace']
